a083d2ce713bbbe2e956d35faef6391cae6edaa8
mariela1217 <mt2590987@gmail.com>
2025-05-21 14:13:47 -0600

    registro de semestres

--- backend/models/periodomodel.js
+++ backend/models/periodomodel.js
@@ -13,31 +13,35 @@ const Periodo = {
   },
 
   // CREATE
-  create: (nuevoPeriodo, callback) => {
-    const query = 'INSERT INTO periodo_academico (fecha_inicio, fecha_fin) VALUES (?, ?)';
-    console.log('Query SQL:', query);
-    console.log('Valores:', [
-      nuevoPeriodo.fecha_inicio,
-      nuevoPeriodo.fecha_fin
-    ]);
-  
-    db.query(query, [
-      nuevoPeriodo.fecha_inicio,
-      nuevoPeriodo.fecha_fin
-    ], (err, results) => {
-      if (err) {
-        console.error(' ERROR SQL:', err); 
-        return callback(err);
-      }
-      callback(null, results);
-    });
-  },
+// CREATE
+create: (nuevoPeriodo, callback) => {
+  const query = 'INSERT INTO periodo_academico (nombre, fecha_inicio, fecha_fin) VALUES (?, ?, ?)';
+  console.log('Query SQL:', query);
+  console.log('Valores:', [
+    nuevoPeriodo.nombre,
+    nuevoPeriodo.fecha_inicio,
+    nuevoPeriodo.fecha_fin
+  ]);
+
+  db.query(query, [
+    nuevoPeriodo.nombre,
+    nuevoPeriodo.fecha_inicio,
+    nuevoPeriodo.fecha_fin
+  ], (err, results) => {
+    if (err) {
+      console.error(' ERROR SQL:', err); 
+      return callback(err);
+    }
+    callback(null, results);
+  });
+}
+,
   
 
   // UPDATE
   update: (id, datosActualizados, callback) => {
-    const query = 'UPDATE periodo_academico SET fecha_inicio = ?, fecha_fin = ? WHERE id_periodo = ?';
-    db.query(query, [datosActualizados.fecha_inicio, datosActualizados.fecha_fin, id], (err, results) => {
+    const query = 'UPDATE periodo_academico SET nombre = ?, fecha_inicio = ?, fecha_fin = ? WHERE id_periodo = ?';
+    db.query(query, [datosActualizados.nombre, datosActualizados.fecha_inicio, datosActualizados.fecha_fin, id], (err, results) => {
       if (err) return callback(err);
       callback(null, results);
     });
