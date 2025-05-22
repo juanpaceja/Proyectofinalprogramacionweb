f16d769fe303fa424b80e1d30451f0b07cd66388
mariela1217 <mt2590987@gmail.com>
2025-05-21 21:57:52 -0600

    registro alumnos

--- backend/routes/gruporoutes.js
+++ backend/routes/gruporoutes.js
@@ -8,6 +8,7 @@ router.get('/grupo', grupoController.getGroup);
 router.post('/grupo', grupoController.createGroup);
 router.put('/grupo/:id', grupoController.updateGroup);
 router.delete('/grupo/:id', grupoController.deleteGroup);
+router.get('/grupo/carrera', grupoController.getGruposPorCarrera);
 
 
 module.exports = router;
