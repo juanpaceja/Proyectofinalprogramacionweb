d25861156305dd009f9fc47f7fc043909225eb05
Valentina <valecasstle@gmail.com>
2025-05-27 23:45:23 -0600

    fixeddesign

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const conexion= mysql.createConnection({
  host: 'localhost',  
  user: 'root',
- password: 'holasoyXD1',
+ password: 'Seven707',
  database: 'syscalif'
 });
 conexion.connect((err) => {
